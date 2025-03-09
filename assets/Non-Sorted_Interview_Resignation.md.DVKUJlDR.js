081495e29eb49208c0aa857ed43c218c219be508
Prabhu K H <25prabhu10@gmail.com>
2025-03-08 23:59:46 -0500

    deploy

---
+++ assets/Non-Sorted_Interview_Resignation.md.DVKUJlDR.js
@@ -0,0 +1,38 @@
+import{_ as n,c as s,o as e,ag as t}from"./chunks/framework.CTZL_gMF.js";const u=JSON.parse('{"title":"Resignation","description":null,"frontmatter":{"title":"Resignation","description":null,"date":"2023-01-09T00:00:00.000Z","lastmod":"2023-01-09T00:00:00.000Z"},"headers":[],"relativePath":"Non-Sorted/Interview/Resignation.md","filePath":"Non-Sorted/Interview/Resignation.md","lastUpdated":1690829560000}'),p={name:"Non-Sorted/Interview/Resignation.md"};function i(o,a,l,r,c,d){return e(),s("div",null,a[0]||(a[0]=[t(`<h1 id="resignation" tabindex="-1">Resignation <a class="header-anchor" href="#resignation" aria-label="Permalink to &quot;Resignation&quot;">​</a></h1><div class="language-text vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">text</span><pre class="shiki shiki-themes github-light vitesse-dark vp-code" tabindex="0"><code><span class="line"><span>Dear ,</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Please accept this letter as formal notification that I am resigning from Tata Consultancy Services today, the 9th Jan 2023.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Please let me know how I can be of assistance during this transition.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>I am really thankful to TCS for giving me good opportunities and experience in the last 6 years. I will always treasure the support and guidance provided by my team members. I feel that it is time for a new challenge and experience. Please accept my resignation.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Sincerely,</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Prabhu K Hiremath</span></span>
+<span class="line"><span>+91 9480580490</span></span></code></pre></div><p>Samples:</p><div class="language-text vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">text</span><pre class="shiki shiki-themes github-light vitesse-dark vp-code" tabindex="0"><code><span class="line"><span>Dear ,</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Please accept this letter as formal notification that I am resigning from my position as Account Executive with Marketing Media. My last day will be Thursday, March 2.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Thank you so much for the opportunity to work in this position for the past six years. I&#39;ve greatly enjoyed and appreciated the opportunities I&#39;ve had to grow our sales team and pipeline, be a part of creating a great product, and work with my fellow colleagues, and I&#39;ve learned so much about marketing strategy and the digital media space, which I will certainly take with me throughout my career.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>I&#39;ll do everything possible to wrap up my duties and train other team members over the next two weeks. Please let me know if there&#39;s anything else I can do to help during this transition.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>I wish the company continued success, and I hope to stay in touch in the future.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Sincerely,</span></span></code></pre></div><div class="language-text vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">text</span><pre class="shiki shiki-themes github-light vitesse-dark vp-code" tabindex="0"><code><span class="line"><span>Subject: Notice of Resignation – Your Name</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Dear Ms Gupta</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>I feel sorry to tell you that I am resigning from my position of Digital Marketing Manager at your company and will be effectual from 1st July. I&#39;ve been presented for a position at XYZ Company and have resolute in accepting. I&#39;ve enjoyed my instance here, and I value all that you have done for me. Without this job, I wouldn&#39;t have educated so much about managing and production with employees.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>If you have any queries, please let me know and thank you so much for the moment here. I am confident we&#39;ll see each other at assorted professional measures in the future.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Sincerely,</span></span></code></pre></div><div class="language-text vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">text</span><pre class="shiki shiki-themes github-light vitesse-dark vp-code" tabindex="0"><code><span class="line"><span>Dear Stephen,</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>I would like to formally notify you that I am resigning from my position as Project Manager for the XYZ Company, w.e.f 1st June.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Thank you so much for the opportunities for expert and personal growth that you have provided me for the duration of the last six years. I have liked working for the company, and I am grateful for the support provided me through my possession with the company.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>If I can lend a hand during this transition, please let me know.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Sincerely,</span></span></code></pre></div>`,6)]))}const m=n(p,[["render",i]]);export{u as __pageData,m as default};
